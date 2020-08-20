f94b36763a0a5721b4ad0bce4a8677d1369d678c
SkieHacker <40538401+SkieAdmin@users.noreply.github.com>
2020-08-20 09:40:45 +0800

    Update PandaCMD.cmd

--- PandaCMD.cmd
+++ PandaCMD.cmd
@@ -4,4 +4,9 @@ title [ Panda Command Host ] for v1.1
 echo [ Panda Command Host ]
 echo --------------------------------------------
 npm start
+echo -------------------------------------------
+echo [ Obfuscated Complete, Returning Message to Application ]
+echo some-text  > filename.txt
+timeout 2>nul
+cls
 exit
